86b49b2e40b48160f8f56f4a5cda87c0050bf4ea
DevHyperCoder <devan@devhypercoder.com>
2022-05-07 14:26:09 +0530

    Implement stub auth

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { OauthCallbackPage } from "./pages/OauthCallbackPage";
 import { SubmissionPage } from "./pages/SubmissionPage";
@@ -8,11 +9,33 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/submission" element={<SubmissionPage />} />
+        <Route path="/submission" element={<RequireAuth><SubmissionPage /></RequireAuth>} />
         <Route path="/oauth/callback" element={<OauthCallbackPage />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+
+function RequireAuth({ children }: { children: JSX.Element }) {
+  const [auth,setAuth] = useState<string | null>(null);
+  const [pending,setPending] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAuth(null);
+      setPending(false);
+    },400)
+  }, []);
+
+  if (pending){ return null}
+
+  if (auth) {
+    return children;
+  } else {
+    return <Navigate to="/" />;
+  }
+}
+
 
 export default App;
