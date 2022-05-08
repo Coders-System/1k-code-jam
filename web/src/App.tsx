b3c850eb581962c8fa85c721bf52393bce896bbc
DevHyperCoder <devan@devhypercoder.com>
2022-05-08 18:16:59 +0530

    Add 404 page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { authContext, getLoggedInUser } from "./http/auth";
 import { User } from "./http/types";
+import { NotFoundPage } from "./pages/404";
 import { HomePage } from "./pages/HomePage";
 import { OauthCallbackPage } from "./pages/OauthCallbackPage";
 import { SubmissionPage } from "./pages/SubmissionPage";
@@ -25,6 +26,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/dashboard" element={<SubmissionPage />} />
           <Route path="/oauth/callback" element={<OauthCallbackPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
     </authContext.Provider>
---
+++ web/src/pages/404.tsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import { Footer } from "../components/Footer";
+import { Navbar } from "../components/Navbar";
+import { getOAuthCallbackURL } from "../http/auth";
+
+export function NotFoundPage() {
+  const [oauthURL, setOauthUrl] = useState<string | null>();
+
+  useEffect(() => {
+    getOAuthCallbackURL().then((url) => setOauthUrl(url));
+  }, []);
+
+  return (
+      <div className="h-screen flex items-center flex-col justify-center ">
+        <Navbar oauthURL={oauthURL} />
+        <div className="my-auto w-full flex flex-col gap-3 items-center justify-center">
+            <h1 className="text-center text-4xl ">404 Page Not Found </h1>
+            <a  className="underline underline-offset-2" href="/">Go home</a>
+        </div>
+        <Footer />
+      </div>
+  )
+}
