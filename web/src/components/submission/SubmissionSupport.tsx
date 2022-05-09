8e9fa0cc408589ceb13af613062a31c23c2745de
ahnaf-zamil <ahnaf@ahnafzamil.com>
2022-05-09 20:11:10 +0600

    removed support button

--- web/src/components/submission/SubmissionSupport.tsx
+++ web/src/components/submission/SubmissionSupport.tsx
@@ -4,7 +4,7 @@ export function SubmissionSupport() {
   return (
     <div className="mt-16 mb-4 flex flex-row items-center justify-center gap-3">
       <p className="text-center font-bold">Having issues?</p>
-      <Button className="h-min ">Contact support</Button>
+      DM Coder's Bot regarding the issue
     </div>
   );
 }
